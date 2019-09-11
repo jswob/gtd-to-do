985a0242bdde11664cb775b2332fc8b5636ddfc4
jacek swoboda <jacswob@gmail.com>
2019-09-11 22:39:41 +0200

    create acceptance-tests for sign-up and sign-in routes

---
+++ tests/acceptance/unauth/sign-in-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+
+module('Acceptance | unauth/sign in', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should redirect to auth.collection route if user is logged in', async function(assert) {
+    await visit('/unauth/sign-in');
+
+    await authenticateSession();
+
+    assert.equal(currentURL(), '/auth/collection');
+  });
+});
