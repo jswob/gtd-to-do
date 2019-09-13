fa6a374084991b8030936a42dad4cea7693bc965
jacek swoboda <jacswob@gmail.com>
2019-09-13 22:21:58 +0200

    create adapter for user model

--- app/models/user.js
+++ app/models/user.js
@@ -15,14 +15,14 @@ const Validations = buildValidations({
   password: {
     description: "Password",
     validators: [
-      validator("format", {
-        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/,
-        message:
-          "Password must include at least one upper case letter, one lower case letter, and a number"
-      }),
       validator("length", {
         min: 6,
         max: 15
+      }),
+      validator("format", {
+        regex: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$/,
+        message:
+          "Password must include at least one upper case letter, one lower case letter, and a number"
       })
     ]
   }
