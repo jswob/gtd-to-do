fe5d3ae6fe52ff4a58e7fd3b18deef21006337ee
jacek swoboda <jacswob@gmail.com>
2019-09-28 20:48:58 +0200

    repair eslint errors

--- tests/integration/components/collection-editor-test.js
+++ tests/integration/components/collection-editor-test.js
@@ -1,6 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render, fillIn, click, pauseTest } from "@ember/test-helpers";
+import { render, fillIn, click } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 import $ from "jquery";
 
